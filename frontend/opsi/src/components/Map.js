0e165836b968336273dfeb21ca655b7b99547cbc
OgrizekLuka <luka.ogrizek@student.um.si>
2021-05-30 12:49:25 +0200

    Fixed map.js

--- frontend/opsi/src/components/Map.js
+++ frontend/opsi/src/components/Map.js
@@ -1,5 +1,8 @@
-import React, {useState, useEffect, useMemo, useCallback } from 'react';
+import React, { useState, useEffect, useMemo, useCallback } from 'react';
 import ReactMapGL, { Layer, Source } from 'react-map-gl';
+import Container from 'react-bootstrap/Container'
+import DropdownButton from 'react-bootstrap/DropdownButton'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 
 export default function MapRegije() {
@@ -9,7 +12,7 @@ export default function MapRegije() {
         longitude: 14.815333333333333,
         width: '100%',
         height: '85vh',
-        zoom: 8
+        zoom: 7
     });
 
     const [allData, setAllData] = useState(null);
@@ -18,8 +21,8 @@ export default function MapRegije() {
         fetch(
             'https://raw.githubusercontent.com/stefanb/gurs-obcine/master/data/SR.geojson'
         )
-        .then(resp => resp.json())
-        .then(json => setAllData(json));
+            .then(resp => resp.json())
+            .then(json => setAllData(json));
     }, []);
 
     const data = useMemo(() => {
@@ -30,7 +33,7 @@ export default function MapRegije() {
         id: 'data',
         type: 'fill',
     };
-    
+
     const [color, setColor] = useState('#8cc08c')
 
     const [hoverInfo, setHoverInfo] = useState(null);
@@ -38,7 +41,7 @@ export default function MapRegije() {
     const onHover = useCallback(event => {
         const {
             features,
-            srcEvent: {offsetX, offsetY}
+            srcEvent: { offsetX, offsetY }
 
         } = event;
 
@@ -46,40 +49,53 @@ export default function MapRegije() {
 
         setHoverInfo(
             hoveredFeature
-            ? {
-                feature: hoveredFeature,
-                x: offsetX,
-                y: offsetY
-            }
-            : null
+                ? {
+                    feature: hoveredFeature,
+                    x: offsetX,
+                    y: offsetY
+                }
+                : null
         );
     }, []);
-       
-    
+
+
 
     return (
-    <div>
 
-        <ReactMapGL 
-        {...viewport}
-        mapboxApiAccessToken={"pk.eyJ1Ijoibmlra292YWNldmljIiwiYSI6ImNrcDlwajBjaDBnbmEycmxsMDU5bHZtZWIifQ.7jC2o5D5GqDT7NCqCCkufQ"}
-        mapStyle={"mapbox://styles/nikkovacevic/ckp9xo2vn1j0g17o7s9eealzm"}
-        onViewportChange={viewport => {
-            setViewport(viewport);
-        }}
-        interactiveLayerIds={['data']}
-        onHover={onHover}
-        >
-        
-            <Source id="sourcelayer" type="geojson" data={data}>
-                <Layer {...layerStyle} paint={{'fill-color': color, 'fill-opacity': 0.3, 'fill-outline-color': "#45634d"}} />
-            </Source>
-            {hoverInfo && (
-                <div className="tooltip123123" style={{left: hoverInfo.x, top: hoverInfo.y}}>
-                <div>{hoverInfo.feature.properties.SR_UIME}</div>
-                </div>
-            )}
-        </ReactMapGL>
-    </div>
+
+
+        <Container>
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                <Dropdown.Item href="/mapobcine">Karta obcin</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+            </DropdownButton>
+
+
+
+            <div>
+                <ReactMapGL
+                    {...viewport}
+                    mapboxApiAccessToken={"pk.eyJ1Ijoibmlra292YWNldmljIiwiYSI6ImNrcDlwajBjaDBnbmEycmxsMDU5bHZtZWIifQ.7jC2o5D5GqDT7NCqCCkufQ"}
+                    mapStyle={"mapbox://styles/nikkovacevic/ckp9xo2vn1j0g17o7s9eealzm"}
+                    onViewportChange={viewport => {
+                        setViewport(viewport);
+                    }}
+                    interactiveLayerIds={['data']}
+                    onHover={onHover}
+                >
+
+                    <Source id="sourcelayer" type="geojson" data={data}>
+                        <Layer {...layerStyle} paint={{ 'fill-color': color, 'fill-opacity': 0.3, 'fill-outline-color': "#45634d" }} />
+                    </Source>
+                    {hoverInfo && (
+                        <div className="tooltip123123" style={{ left: hoverInfo.x, top: hoverInfo.y }}>
+                            <div>{hoverInfo.feature.properties.SR_UIME}</div>
+                        </div>
+                    )}
+                </ReactMapGL>
+            </div>
+
+        </Container>
     );
 }
