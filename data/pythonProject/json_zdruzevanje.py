1f6e5b59f6f3e0c2f106edbb6678c2b28482baa1
Aljaz <aljaz.neuberg@student.um.si>
2021-06-01 19:45:21 +0200

    Zdruzen json za obcine

--- data/pythonProject/json_zdruzevanje.py
+++ data/pythonProject/json_zdruzevanje.py
@@ -27,3 +27,33 @@ for i in range(12):
 
 with open(r"C:\Users\Miki\Desktop\regije_zdruzen.json", "w", encoding='utf-8') as jsonfile:
     json.dump(map, jsonfile, ensure_ascii=False, indent=4)
+	
+	
+	
+"///////////////////"
+
+
+with open('C:\Opsi\data\Json\migracije_obcine.json', 'r', encoding='utf-8') as myfile:
+    data_o=myfile.read()
+obc = json.loads(data_o)
+
+"Json map"
+mapa1 = 'https://raw.githubusercontent.com/stefanb/gurs-obcine/master/data/OB.geojson'
+j = requests.get(url=mapa1)
+map_o = json.loads(j.content)
+
+
+
+
+array_n = []
+for i in range(212):
+    mapa_obcine = map_o["features"][i]["properties"]['OB_UIME']
+    for j in obc:
+        if (j["OBČINE"] == mapa_obcine):
+            array_n.append(j)
+            map_o["features"][i]["podatki_obcine"] = array_n
+    array_n = []
+
+
+with open(r"C:\Users\Miki\Desktop\obcine_zdruzene.json", "w", encoding='utf-8') as jsonfile_o:
+    json.dump(map_o, jsonfile_o, ensure_ascii=False, indent=4)
