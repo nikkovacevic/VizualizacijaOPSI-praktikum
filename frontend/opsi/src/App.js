7896496c54a83286871b6558dc8a4af452d0b3dd
Aljaz <aljaz.neuberg@student.um.si>
2021-06-15 17:06:13 +0200

    barvanje obcin

--- frontend/opsi/src/App.js
+++ frontend/opsi/src/App.js
@@ -13,7 +13,7 @@ import About from './components/About';
 import MapRegije from './components/Map';
 import MapObcine from './components/MapObcine';
 import Footer from './components/Footer';
-
+import Poskus from './components/poskus';
 
 
 function App() {
@@ -32,6 +32,8 @@ function App() {
       <Route path='/onas' component={About} />
 
       <Route path='/statistika' component={Dashboard} />
+
+      <Route path='/poskus' component={Poskus} />
 
       </Switch>
       <Footer/>
---
+++ frontend/opsi/src/components/samofor/control-panel-obcine.js
@@ -0,0 +1,25 @@
+import * as React from 'react';
+import '../css/styles.css';
+
+function ControlPanelObcine(props) {
+  const { year } = props;
+
+  return (
+    <div className="control-panel">
+      <label><b>Leto:{year}</b></label>
+      <div key={'year'} className="input">
+        <input
+          className="input"
+          type="range"
+          value={year}
+          min={2000}
+          max={2020}
+          step={1}
+          onChange={evt => props.onChange(evt.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default React.memo(ControlPanelObcine);
